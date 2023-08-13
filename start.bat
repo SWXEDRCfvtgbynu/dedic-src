98cb40ebe76ce1fc3f6b0f320137356d06258f24
SWXEDRCfvtgbynu <139407227+SWXEDRCfvtgbynu@users.noreply.github.com>
2023-08-14 01:37:52 +0300

    Add files via upload

--- start.bat
+++ start.bat
@@ -15,6 +15,6 @@ echo если сдох - ребилд
 echo ип:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 echo юзер: dlbb
-echo пасс: DISPVPS
+echo пасс: @n91d-DiePls!
 echo hf 
 ping -n 10 127.0.0.1 >nul

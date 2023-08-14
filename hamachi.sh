0cd9a8f591c4c6561f5bb6128f26283be5221101
SWXEDRCfvtgbynu <139407227+SWXEDRCfvtgbynu@users.noreply.github.com>
2023-08-14 03:10:03 +0300

    Add files via upload

--- hamachi.sh
+++ hamachi.sh
@@ -1,6 +1,6 @@
 curl https://raw.githubusercontent.com/SWXEDRCfvtgbynu/dedic-src/main/hamachi.deb -o hamachi.deb & pid=$!
 wait pid
-sudo dpkg --install hamachi & pid=$!
+sudo dpkg --install hamachi.deb & pid=$!
 wait pid
 hamachi login & pid=$!
 wait pid
